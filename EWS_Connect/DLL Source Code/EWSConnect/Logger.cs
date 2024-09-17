24f0db0ea304ee9a288cd4e0cc0b38a06c5372df
Sikha P <sikha.p@automationanywhere.com>
2024-09-17 16:41:58 +0530

    EWS Connect updates

---
+++ EWS_Connect/DLL Source Code/EWSConnect/Logger.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace EWSConnect
+{
+    class Logger
+    {
+        public static void LogToFile(string line)
+        {
+            string logFile = "EWSConnectAndMoveFilteredEmails.log";
+            string logFileFolderPath = getLogFolderPath();
+
+            DateTime dateNow = DateTime.Now;
+            //2022 - Mar - 23 Wed 09:09:50.623
+            string now = dateNow.ToString("yyyy-MMM-dd ddd HH:mm:ss");
+
+            if (!File.Exists(logFileFolderPath + logFile))
+            {
+                if (!Directory.Exists(logFileFolderPath))
+                {
+                    Directory.CreateDirectory(logFileFolderPath);
+                }
+                File.Create(logFileFolderPath + logFile).Dispose();
+
+                using (TextWriter tw = new StreamWriter(logFileFolderPath + logFile, true))
+                {
+                    tw.WriteLine(now + ": " + line);
+                }
+
+            }
+            else if (File.Exists(logFileFolderPath + logFile))
+            {
+                using (TextWriter tw = new StreamWriter(logFileFolderPath + logFile, true))
+                {
+                    tw.WriteLine(now + ": " + line);
+                }
+            }
+        }
+
+        public static string getLogFolderPath()
+        {
+            string AAInstallationPath = checkInstalled("Automation Anywhere Bot Agent");
+            XmlDocument xml = new XmlDocument();
+            string xmlFilePath = AAInstallationPath + @"config\nodemanager-logging.xml";
+            xml.Load(xmlFilePath);
+            //XmlNodeList nodelist = xml.SelectNodes("//Property[@name='logPath']");
+            XmlNode node = xml.SelectSingleNode("//Property[@name='logPath']");
+            string logpath = node.InnerText + "/";
+            if (logpath.Contains("${env:PROGRAMDATA}"))
+            {
+                string pgrmDataEnv = Environment.GetEnvironmentVariable("PROGRAMDATA");
+                logpath = logpath.Replace("${env:PROGRAMDATA}", pgrmDataEnv);
+                logpath = logpath.Replace("\\", "/");
+            }
+            return logpath;
+        }
+        private static string checkInstalled(string findByName)
+        {
+            string displayName;
+            string InstallPath;
+            string registryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+
+            //64 bits computer
+            RegistryKey key64 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            RegistryKey key = key64.OpenSubKey(registryKey);
+
+            if (key != null)
+            {
+                foreach (RegistryKey subkey in key.GetSubKeyNames().Select(keyName => key.OpenSubKey(keyName)))
+                {
+                    displayName = subkey.GetValue("DisplayName") as string;
+                    if (displayName != null && displayName.Contains(findByName))
+                    {
+
+                        InstallPath = subkey.GetValue("InstallLocation").ToString();
+
+                        return InstallPath; //or displayName
+
+                    }
+                }
+                key.Close();
+            }
+
+            return null;
+        }
+    }
+
+}
--- EWS_Connect/README-Template.md
+++ EWS_Connect/README-Template.md
@@ -1,10 +1,11 @@
 # A DLL to connect to Exchange Web Service(EWS) Server using client credentials
 
-A DLL to Connect to a EWS Server using client credentials grant flow and Read the Unread emails with content includes foriegn characters. 
+
+A DLL to connect to an EWS server using client credentials grant flow, read all/read/unread emails, filter out the emails that match the provided regex pattern with its subject, and move those emails to a specified target folder.
 
 ## Description
 
-This DLL can connect to EWS Server using client credentials grant flow and Read emails with content includes foriegn characters. You can use this inside the A360 Bot.
+You can use this DLL inside the A360 Bot.Please find the exported bot from the same repository.
 There are 2 functions inside the dll.
 
 * Connect
@@ -15,15 +16,24 @@ There are 2 functions inside the dll.
     * exchangeVersion (eg: Exchange2010_SP2)
                       (https://learn.microsoft.com/en-us/dotnet/api/microsoft.exchange.webservices.data.exchangeversion?view=exchange-ews-api)
 
-* GetUnReadEmails
+* GetEmails
+    * filterCondition ( options : ALL/READ/UNREAD)
     * folderPath   (eg: Inbox/Test/Test2)
- 
+    * targetFolderForFilteredItems  (eg: Inbox/Test/Test2)
+    * filterRegexPattern   (eg : [^\x00-\x7F] )
+
+
+       If you pass the "filterRegexPattern" value as empty string it will run with the default regex configured inside the DLL. Please find the default regex below. This has been generated as per the characters specified in the following Automation Anywhere documenation https://docs.automationanywhere.com/bundle/enterprise-v2019/page/enterprise-cloud/topics/aae-client/bot-creator/commands/cloud-using-connect-action.html
+
+[\x00-\x08\x0B\x0C\x0E-\x1F]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uFDD0-\uFDEF]|[\uFFFE\uFFFF]|[\uD83F\uDFFE\uD83F\uDFFF]|[\uD87F\uDFFE\uD87F\uDFFF]|[\uD8BF\uDFFE\uD8BF\uDFFF]|[\uD8FF\uDFFE\uD8FF\uDFFF]|[\uD93F\uDFFE\uD93F\uDFFF]|[\uD97F\uDFFE\uD97F\uDFFF]|[\uD9BF\uDFFE\uD9BF\uDFFF]|[\uD9FF\uDFFE\uD9FF\uDFFF]|[\uDA3F\uDFFE\uDA3F\uDFFF]|[\uDA7F\uDFFE\uDA7F\uDFFF]|[\uDABF\uDFFE\uDABF\uDFFF]|[\uDAFF\uDFFE\uDAFF\uDFFF]|[\uDB3F\uDFFE\uDB3F\uDFFF]|[\uDB7F\uDFFE\uDB7F\uDFFF]|[\uDBBF\uDFFE\uDBBF\uDFFF]|[\uDBFF\uDFFE\uDBFF\uDFFF]
+
       
 ### Dependencies
 
-* Microsoft.Exchange.WebServices.Data
-* Microsoft.Identity.Clien
-* Newtonsoft.Json
+* Mnt.Microsoft.Exchange.WebServices.Data.dll
+* Microsoft.Identity.Client.dll
+* Microsoft.IdentityModel.Abstractions.dll
+
 
 
 ![image](https://github.com/user-attachments/assets/1ad002a7-66df-49d5-a5c9-c0bc97cc2dd6)
